380fd63fe2a35a89e030d77ae6e4bd44300783a2
unknown <30215@JK.local>
2019-08-26 17:45:13 +0900

    add AI_to_random_play.py and access_colab.bat

---
+++ AI_to_random_play.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from pv_mcts import pv_mcts_action
+from tensorflow.keras.models import load_model
+from game import *
+import random
+if __name__ == '__main__':
+    # 状態の生成
+    state = State()
+    model = load_model('./model/best.h5')
+    next_action = pv_mcts_action(model, 0.0)
+
+    AI_color = random.randint(0,1)
+    random_color = None
+    if AI_color == 0:
+        random_color = 1
+    else:
+        random_color = 0
+
+    count = 0
+        
+    # ゲーム終了までのループ
+    while True:
+        # ゲーム終了時
+        if state.is_done():
+            break
+        if (count % 2) == random_color:
+            # 次の状態の取得
+            state = state.next(random_action(state))
+        else:
+            state = state.next(next_action(state))
+    print("黒:{0} 白:{1}".format(state.piece_count(state.pieces)..format(state.piece_count(state.enemy_pieces))
---
+++ access_colab.bat
@@ -0,0 +1,8 @@
+@echo off
+for /l %%i in (0,1,12) do (
+  echo [%%i] %date% %time% connected.
+  start chrome.exe --new-window https://colab.research.google.com/drive/1SBINIp8MrW6KTmQ6LvvXsoZQadAa0jI_#scrollTo=8Juqgs-tW0wB
+  timeout 3600 /nobreak >nul
+)
+
+pause
