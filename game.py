3c201e27f9a6d26c2f2d98f3ec143ba43162db7b
unknown <30215@JK.local>
2019-08-28 09:50:52 +0900

    create bitboard branch

--- game.py
+++ game.py
@@ -5,6 +5,7 @@
 # パッケージのインポート
 import random
 import math
+import gmpy2
 
 # ゲーム状態
 class State:
@@ -17,45 +18,39 @@ class State:
         self.pass_end = False
 
         # 石の配置
-        self.pieces = pieces
-        self.enemy_pieces = enemy_pieces
+        self.black_board = pieces
+        self.white_board = enemy_pieces
         self.depth = depth
 
         # 石の初期配置
         if pieces == None or enemy_pieces == None:
-            self.pieces = [0] * 64
-            self.pieces[28] = self.pieces[35] = 1
-            self.enemy_pieces = [0] * 64
-            self.enemy_pieces[27] = self.enemy_pieces[36] = 1
+            self.black_board = 0x0000001008000000
+            self.white_board = 0x0000000810000000
 
     # 石の数の取得
-    def piece_count(self, pieces):
-        count = 0
-        for i in pieces:
-            if i == 1:
-                count +=  1
-        return count
+    def piece_count(self, board):
+        return gmpy2.popcount(black_board)
 
     # 負けかどうか
     def is_lose(self):
-        return self.is_done() and self.piece_count(self.pieces) < self.piece_count(self.enemy_pieces)
+        return self.is_done() and self.piece_count(self.black_board) < self.piece_count(self.white_board)
 
     # 引き分けかどうか
     def is_draw(self):
-        return self.is_done() and self.piece_count(self.pieces) == self.piece_count(self.enemy_pieces)
+        return self.is_done() and self.piece_count(self.black_board) == self.piece_count(self.white_board)
 
     # ゲーム終了かどうか
     def is_done(self):
-        return self.piece_count(self.pieces) + self.piece_count(self.enemy_pieces) == 64 or self.pass_end
+        return self.piece_count(self.black_board) + self.piece_count(self.white_board) == 64 or self.pass_end
 
     # 次の状態の取得
     def next(self, action):
-        state = State(self.pieces.copy(), self.enemy_pieces.copy(), self.depth+1)
+        state = State(self.black_board.copy(), self.white_board.copy(), self.depth+1)
         if action != 64:
             state.is_legal_action_xy(action%8, int(action/8), True)
-        w = state.pieces
-        state.pieces = state.enemy_pieces
-        state.enemy_pieces = w
+        w = state.black_board
+        state.black_board = state.white_board
+        state.white_board = w
 
         # 2回連続パス判定
         if action == 64 and state.legal_actions() == [64]:
@@ -80,38 +75,38 @@ class State:
             # １つ目 相手の石
             x, y = x+dx, y+dy
             if y < 0 or 7 < y or x < 0 or 7 < x or \
-                self.enemy_pieces[x+y*8] != 1:
+                self.white_board[x+y*8] != 1:
                 return False
 
             # 2つ目以降
             for j in range(8):
                 # 空
                 if y < 0 or 7 < y or x < 0 or 7 < x or \
-                    (self.enemy_pieces[x+y*8] == 0 and self.pieces[x+y*8] == 0):
+                    (self.white_board[x+y*8] == 0 and self.black_board[x+y*8] == 0):
                     return False
 
                 # 自分の石
-                if self.pieces[x+y*8] == 1:
+                if self.black_board[x+y*8] == 1:
                     # 反転
                     if flip:
                         for i in range(8):
                             x, y = x-dx, y-dy
-                            if self.pieces[x+y*8] == 1:
+                            if self.black_board[x+y*8] == 1:
                                 return True
-                            self.pieces[x+y*8] = 1
-                            self.enemy_pieces[x+y*8] = 0
+                            self.black_board[x+y*8] = 1
+                            self.white_board[x+y*8] = 0
                     return True
                 # 相手の石
                 x, y = x+dx, y+dy
             return False
 
         # 空きなし
-        if self.enemy_pieces[x+y*8] == 1 or self.pieces[x+y*8] == 1:
+        if self.white_board[x+y*8] == 1 or self.black_board[x+y*8] == 1:
             return False
 
         # 石を置く
         if flip:
-            self.pieces[x+y*8] = 1
+            self.black_board[x+y*8] = 1
 
         # 任意の位置が合法手かどうか
         flag = False
@@ -130,9 +125,9 @@ class State:
         str = ''
         for i in range(64):
             str += '  '
-            if self.pieces[i] == 1:
+            if self.black_board[i] == 1:
                 str += ox[0]
-            elif self.enemy_pieces[i] == 1:
+            elif self.white_board[i] == 1:
                 str += ox[1]
             else:
                 str += 'ー'
